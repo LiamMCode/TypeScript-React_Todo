3d95d296c1146a6822f386c822f6d830ddb3fbc1
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-06-08 09:42:58 +0100

    configured babel still cant get jsx to work properly

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,26 @@ module.exports = {
     devtool: 'inline-source-map',
     entry: './public/scripts/App.js',
     output: {
-      path: __dirname + '/src',
-      filename: './public/index.html'
+      path: __dirname + '/public',
+      filename: './public/index.html', 
+      publicPath: "/"
     },
     resolve: {
-      extensions: ['.ts', '.tsx', '.js'], 
+      extensions: ['.ts', '.tsx', '.js', '.jsx'], 
     },
     module: {
       rules: [
-        {test: /\.tsx?$/, loader: 'ts-loader'}
+        {
+          test: /\.tsx?$/, //loader: 'ts-loader', 
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+            }
+          }
+        }
       ]
     }
   }
